fbd55e44ada36a893e2800f492f4d15d537cebf2
zhangxinhua <137330275@qq.com>
2018-04-20 11:42:41 +0800

    Initial commit

---
+++ zxh_customview/src/main/java/com/zxh/customview/VerticalArticleView.java
@@ -0,0 +1,28 @@
+package com.zxh.customview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Joeshould on 2018/4/20.
+ */
+
+public class VerticalArticleView extends LinearLayout {
+    public VerticalArticleView(Context context) {
+        super(context);
+    }
+
+    public VerticalArticleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VerticalArticleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public VerticalArticleView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
